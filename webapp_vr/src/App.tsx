0d77cf6b7f969ba4f5299569d56cd66854efade6
nuzer05ive <143165695+nuzer05ive@users.noreply.github.com>
2025-08-18 19:51:39 -0700

    feat: add Phi-Jostle Monocle spiral engine

--- webapp_vr/src/App.tsx
+++ webapp_vr/src/App.tsx
@@ -1,21 +1,9 @@
-import React, { useState } from 'react';
-import Home from './routes/Home';
-import VRPortal, { GhostProvider, useGhost } from './routes/VRPortal';
-import GhostHUD from './components/GhostHUD';
-
-function HUDOverlay(){
-  const { state } = useGhost();
-  return <GhostHUD {...state} />;
-}
-
-export default function App() {
-  const [route, setRoute] = useState<'home' | 'vr'>('home');
-  return route === 'home' ? (
-    <Home enterPortal={() => setRoute('vr')} />
-  ) : (
-    <GhostProvider>
-      <HUDOverlay />
-      <VRPortal goHome={() => setRoute('home')} />
-    </GhostProvider>
+import React from "react";
+import PhiMonocleRoute from "./routes/PhiMonocleRoute";
+export default function App(){
+  return (
+    <div style={{width:"100%",height:"100%",background:"#060a10"}}>
+      <PhiMonocleRoute/>
+    </div>
   );
 }
